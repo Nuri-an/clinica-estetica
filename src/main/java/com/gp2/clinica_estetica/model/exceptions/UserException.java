e7a9033da56a8aa08a4c78e6a5efe6e8b5c64399
Nurian Coelho <nuriancoelho@hotmail.com>
2022-12-01 14:23:02 -0300

    feat: user exception

---
+++ src/main/java/com/gp2/clinica_estetica/model/exceptions/UserException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gp2.clinica_estetica.model.exceptions;
+
+/**
+ *
+ * @author nuria
+ */
+public class UserException extends RuntimeException {
+
+    public UserException(String msg) {
+        super(msg);
+    }
+       
+}
