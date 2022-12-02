d9ca16a055514b859c675d1ac4b045e436c1acd5
Nurian Coelho <nuriancoelho@hotmail.com>
2022-12-02 11:18:47 -0300

    feat: doctor class

---
+++ src/main/java/com/gp2/clinica_estetica/model/Doctor.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gp2.clinica_estetica.model;
+
+import java.io.Serializable;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author nuria
+ */
+@Entity
+public class Doctor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String securityQuestion;
+    private String securityAnswer;
+
+    @OneToOne(mappedBy = "doctor", cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id") // nome para coluna foreign key no banco
+    private User user;
+
+    public Doctor() {
+        User user = new User();
+        this.user = user;
+        this.id = 0;
+        this.securityQuestion = "";
+        this.securityAnswer = "";
+    }
+
+    public Doctor(User user, String securityQuestion, String securityAnswer) {
+        this.user = user;
+        this.securityQuestion = securityQuestion;
+        this.securityAnswer = securityAnswer;
+    }
+
+    /**
+     * @return the securityQuestion
+     */
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    /**
+     * @param securityQuestion the securityQuestion to set
+     */
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    /**
+     * @return the securityAnswer
+     */
+    public String getSecurityAnswer() {
+        return securityAnswer;
+    }
+
+    /**
+     * @param securityAnswer the securityAnswer to set
+     */
+    public void setSecurityAnswer(String securityAnswer) {
+        this.securityAnswer = securityAnswer;
+    }
+
+    /**
+     * @return the user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+}
