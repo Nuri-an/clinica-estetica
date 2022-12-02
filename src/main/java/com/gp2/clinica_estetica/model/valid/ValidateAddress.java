e5ebc5508139ec7d3cc92d52e183ece67410aa5e
Nurian Coelho <nuriancoelho@hotmail.com>
2022-12-02 13:43:58 -0300

    feat: address validate

---
+++ src/main/java/com/gp2/clinica_estetica/model/valid/ValidateAddress.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gp2.clinica_estetica.model.valid;
+
+import com.gp2.clinica_estetica.model.Address;
+import com.gp2.clinica_estetica.model.exceptions.UserException;
+
+/**
+ *
+ * @author nuria
+ */
+public class ValidateAddress {  
+    public Address addressValidate(String zipCode, String street, String neighborhood){
+        Address address = new Address();     
+        
+        if (zipCode.isEmpty()) 
+            throw new UserException("Error - Campo vazio: 'CEP'.");                
+        address.setZipCode(zipCode);  
+        
+        if (street.isEmpty()) 
+            throw new UserException("Error - Campo vazio: 'rua'.");                
+        address.setStreet(street);  
+        
+        if (neighborhood.isEmpty()) 
+            throw new UserException("Error - Campo vazio: 'bairro'.");                
+        address.setNeighborhood(neighborhood);
+
+        return address;
+    }
+}
