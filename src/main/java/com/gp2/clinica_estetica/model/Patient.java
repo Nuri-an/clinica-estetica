4543ee9dc9b253689321943402b2caa03a4cf2c8
Nurian Coelho <nuriancoelho@hotmail.com>
2022-12-10 18:53:50 -0300

    fix: patient class

--- src/main/java/com/gp2/clinica_estetica/model/Patient.java
+++ src/main/java/com/gp2/clinica_estetica/model/Patient.java
@@ -13,7 +13,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.swing.JTable;
 
@@ -26,52 +25,17 @@ public class Patient extends DaoReports implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String name;
-    private String CPF;
-    private String birthDate;
-    private String securityQuestion;
-    private String securityAnswer;
 
     @OneToOne(mappedBy = "patient", cascade = CascadeType.ALL)
-    @JoinColumn(name = "user_id") // nome para coluna foreign key no banco
-    private User user;
-
-    @JoinColumn(name = "phone_id") // nome para coluna foreign key no banco
-    @OneToOne
-    private PhoneNumber phoneNumber;
-
-    @JoinColumn(name = "address_id") // nome para coluna foreign key no banco
-    @ManyToOne
-    private Address address;
+    @JoinColumn(name = "people_id") // nome para coluna foreign key no banco
+    private People people;
     
     public Patient() {
-        this.name = "";
-        this.CPF = "000.000.000-00";
-        this.birthDate = "00/00/0000";
-        this.securityQuestion = "";
-        this.securityAnswer = "";
+        this.id = -1;
     }
     
-    public Patient(String name, String CPF, String birthDate, String securityQuestion,String securityAnswer) {
-        this.name = name;
-        this.CPF = CPF;
-        this.birthDate = birthDate;
-        this.securityQuestion = securityQuestion;
-        this.securityAnswer = securityAnswer;
-    }
-    
-    public Patient(String name, String CPF, String birthDate, PhoneNumber phoneNumber, Address address) {
-        this.name = name;
-        this.CPF = CPF;
-        this.birthDate = birthDate;
-        this.phoneNumber = phoneNumber;
-        this.address = address;
-    }
-    
-    public Patient(User user, String securityQuestion,String securityAnswer) {
-        this.user = user;
-        this.securityQuestion = securityQuestion;
-        this.securityAnswer = securityAnswer;
+    public Patient(People people) {
+        this.people = people;
     }
     
     public void generateAttendancePDF(JTable table, String type) {
@@ -94,115 +58,17 @@ public class Patient extends DaoReports implements Serializable {
     }
 
     /**
-     * @return the name
+     * @return the people
      */
-    public String getName() {
-        return name;
+    public People getPeople() {
+        return people;
     }
 
     /**
-     * @param name the name to set
+     * @param people the people to set
      */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the CPF
-     */
-    public String getCPF() {
-        return CPF;
-    }
-
-    /**
-     * @param CPF the CPF to set
-     */
-    public void setCPF(String CPF) {
-        this.CPF = CPF;
-    }
-
-    /**
-     * @return the birthDate
-     */
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    /**
-     * @param birthDate the birthDate to set
-     */
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    /**
-     * @return the phoneNumber
-     */
-    public PhoneNumber getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    /**
-     * @param phoneNumber the phoneNumber to set
-     */
-    public void setPhoneNumber(PhoneNumber phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    /**
-     * @return the address
-     */
-    public Address getAddress() {
-        return address;
-    }
-
-    /**
-     * @param address the address to set
-     */
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    /**
-     * @return the securityQuestion
-     */
-    public String getSecurityQuestion() {
-        return securityQuestion;
-    }
-
-    /**
-     * @param securityQuestion the securityQuestion to set
-     */
-    public void setSecurityQuestion(String securityQuestion) {
-        this.securityQuestion = securityQuestion;
-    }
-
-    /**
-     * @return the securityAnswer
-     */
-    public String getSecurityAnswer() {
-        return securityAnswer;
-    }
-
-    /**
-     * @param securityAnswer the securityAnswer to set
-     */
-    public void setSecurityAnswer(String securityAnswer) {
-        this.securityAnswer = securityAnswer;
-    }
-
-    /**
-     * @return the user
-     */
-    public User getUser() {
-        return user;
-    }
-
-    /**
-     * @param user the user to set
-     */
-    public void setUser(User user) {
-        this.user = user;
+    public void setPeople(People people) {
+        this.people = people;
     }
     
 }
