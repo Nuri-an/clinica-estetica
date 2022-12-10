cfe1ed5f996b02ff813929aeb9fabe97f48206f2
Nurian Coelho <nuriancoelho@hotmail.com>
2022-12-10 18:59:12 -0300

    feat: procedure class

---
+++ src/main/java/com/gp2/clinica_estetica/model/Procedure.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gp2.clinica_estetica.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author nuria
+ */
+@Entity
+public class Procedure implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Double price;
+    
+    
+    public Procedure() {
+        this.id = -1;
+        this.name = "";
+        this.price = 0.0;
+    }
+    
+    public Procedure(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the price
+     */
+    public Double getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
