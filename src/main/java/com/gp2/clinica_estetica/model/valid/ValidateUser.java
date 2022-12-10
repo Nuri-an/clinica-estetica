a1b8a452b7fe42c9b4be6511b32f6a1be884d203
Nurian Coelho <nuriancoelho@hotmail.com>
2022-12-10 18:58:42 -0300

    fix: validates with people class

--- src/main/java/com/gp2/clinica_estetica/model/valid/ValidateUser.java
+++ src/main/java/com/gp2/clinica_estetica/model/valid/ValidateUser.java
@@ -14,17 +14,49 @@ import com.gp2.clinica_estetica.model.exceptions.UserException;
  */
 public class ValidateUser {
     
-    public User authValidate(String login, String password){
+    public User loginValidate(String login, String password){
         User user = new User();
         if (login.isEmpty())
             throw new UserException("Error - Campo vazio: 'login'.");
+        
+        ValidatePF validCPF = new ValidatePF();
+        if(!validCPF.verifyCPF(login))
+          throw new UserException("Error - CPF para login inválido!");
         user.setLogin(login);
         
         if (password.isEmpty()) 
-            throw new UserException("Error - Campo vazio: 'senha'.");                
+            throw new UserException("Error - Campo vazio: 'senha'.");  
+        if (password.length() < 6 || password.matches("\\d+")) 
+            throw new UserException("Error - Senha inválida.");  
+            
         user.setPassword(password);
 
         return user;
+    }
+    
+    public void registerValidate(String login, String password, String securityQuestion, String securityAnswer){
+        User user = new User();
+        if (login.isEmpty())
+            throw new UserException("Error - Campo vazio: 'login'.");
+        
+        ValidatePF validCPF = new ValidatePF();
+        if(!validCPF.verifyCPF(login))
+          throw new UserException("Error - CPF para login inválido!");
+        user.setLogin(login);
+        
+        if (password.isEmpty()) 
+            throw new UserException("Error - Campo vazio: 'senha'.");  
+        if (password.length() < 6) 
+            throw new UserException("Error - Senha muito pequena.");  
+        if (password.matches("\\d+")) 
+            throw new UserException("Error - Informe ao menos 1 número para a senha.");   
+        
+        if (securityQuestion.isEmpty()) 
+            throw new UserException("Error - Campo vazio: 'pergunta de segurança'.");   
+        
+        if (securityAnswer.isEmpty()) 
+            throw new UserException("Error - Campo vazio: 'resposta de segurança'.");  
+        
     }
     
 }
