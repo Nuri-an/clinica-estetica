f39e8a885756eb9e3be9313d54d40a037013011d
Darloon Lino <darloon.lino.souza@hotmail.com>
2022-12-22 08:04:42 -0300

    feat: update patient table list

--- src/main/java/com/gp2/clinica_estetica/controller/PatientController.java
+++ src/main/java/com/gp2/clinica_estetica/controller/PatientController.java
@@ -5,9 +5,12 @@
  */
 package com.gp2.clinica_estetica.controller;
 
+import com.gp2.clinica_estetica.model.People;
 import com.gp2.clinica_estetica.model.dao.PatientDAO;
 import com.gp2.clinica_estetica.model.exceptions.UserException;
 import com.gp2.clinica_estetica.model.valid.ValidateUser;
+import java.util.List;
+import javax.swing.JTable;
 
 /**
  *
@@ -30,5 +33,11 @@ public class PatientController {
         } catch (UserException e) {
             throw new UserException("Error - Falha ao realizar cadastro.");
         }
+    }
+    
+    
+    public void updateTable(JTable grd) {
+        List<People> listAll = repositorio.findAllPatients();
+        Util.jTableShow(grd, new TMListPatient(listAll), null);
     }
 }
