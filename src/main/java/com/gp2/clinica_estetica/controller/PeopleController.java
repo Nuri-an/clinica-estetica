1e160e132f63a93e6b33fca208aa47181753b29f
Darloon Lino <darloon.lino.souza@hotmail.com>
2022-12-22 11:20:40 -0300

    fix: validate order

--- src/main/java/com/gp2/clinica_estetica/controller/PeopleController.java
+++ src/main/java/com/gp2/clinica_estetica/controller/PeopleController.java
@@ -24,7 +24,6 @@ public class PeopleController {
     public PeopleController() {
         repositorio = new PeopleDAO();
     }
-    
 
     public People onFetchPeople(String CPF) {
         ValidatePF validPF = new ValidatePF();
@@ -42,16 +41,16 @@ public class PeopleController {
     }
 
     public void onBasicRegister(String name, String CPF, String birthDate, String number, boolean isWhatsapp, String zipCode, String street, String neighborhood, Integer houseNumber) {
+        ValidatePeople validPeople = new ValidatePeople();
+        validPeople.basicRegisterValidate(name, CPF, birthDate);
+
+        ValidatePhoneNumber validPhone = new ValidatePhoneNumber();
+        validPhone.phoneNumberValidate(number, isWhatsapp);
+
+        ValidateAddress validAddress = new ValidateAddress();
+        validAddress.addressValidate(zipCode, street, neighborhood, houseNumber);
+        
         try {
-            ValidatePhoneNumber validPhone = new ValidatePhoneNumber();
-            validPhone.phoneNumberValidate(number, isWhatsapp);
-
-            ValidateAddress validAddress = new ValidateAddress();
-            validAddress.addressValidate(zipCode, street, neighborhood, houseNumber);
-
-            ValidatePeople validPeople = new ValidatePeople();
-            validPeople.basicRegisterValidate(name, CPF, birthDate);
-
             repositorio.basicRegister(name, CPF, birthDate, number, isWhatsapp, zipCode, street, neighborhood, houseNumber);
         } catch (UserException e) {
             throw new UserException("Error - Falha ao realizar cadastro de paciente.");
